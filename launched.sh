7844624126d3e7170f97a754a8730020ff1665e5
Titi seb <titisebpi@gmail.com>
2020-04-07 11:50:25 +0200

    add send xml ftp

--- launched.sh
+++ launched.sh
@@ -2,5 +2,7 @@
 date
 /home/jeedom/open2300-1.10/xml2300 /tmp/currentxml.xml &&
 date &&
+/usr/bin/node /home/jeedom/open2300ToJeedom/upFTP.js &&
+date &&
 /usr/bin/node /home/jeedom/open2300ToJeedom/meteo.js &&
 date 
